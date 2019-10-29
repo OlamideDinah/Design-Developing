b726c6546650c40d66412f1c47b383232cedbc07
Y9Design-ODIN <55104062+Y9Design-ODIN@users.noreply.github.com>
2019-10-28 23:50:18 -0400

    simplexample

---
+++ mathbasic.py
@@ -0,0 +1,9 @@
+print ("This program multiplies two numbers")
+
+x = float(input("What is the first value? \n"))
+y = float(input("What is the second value? \n"))
+
+answer = x * y
+
+print("the answer is", answer )
+
